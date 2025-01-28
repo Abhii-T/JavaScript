0222ede8b3872a6ce8b761249963e5cbe54fcaf1
Abhii-T <127598237+Abhii-T@users.noreply.github.com>
2025-01-28 14:46:38 +0000

    commit

---
+++ basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let emailId = "abhishekthore9@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState
+
+
+//accountId= 122443 //can't change the constant that is declared above
+
+console.log(accountId);
+emailId="dcd@gamil.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+
+console.table([accountId,emailId,accountPassword,accountCity,accountState]);
+
+/* prefer not to use var
+ because of issue in block scope and functional scope*/
