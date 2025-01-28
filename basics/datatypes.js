4665ed157dd20499767710e84a317d78a6108395
Abhii-T <127598237+Abhii-T@users.noreply.github.com>
2025-01-28 16:55:10 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as new version
+
+//alert(3+3) //  we are using node js not browser
+
+console.log(3
+    +3 //code readibility shoul be  high(this is written ina wrong way and readibility here is bad)
+)
+
+let name="Abhishek";
+let age=21;
+let isLoggedIn=false;
+
+//number--> 2^53
+//bigInt
+//string -->" "
+//boolean
+//null -->it is a standalone value
+//undefined--> no value assigned yet
+//symbol--> to find uniqness
+
+
+//object
+
+
+console.log(typeof "abhishek") //string
+console.log(typeof 21) //number 
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
