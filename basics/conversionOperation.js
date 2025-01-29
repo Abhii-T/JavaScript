7b4408ee68222d734945c6ecfbd7242f8e1cf2a1
Abhii-T <127598237+Abhii-T@users.noreply.github.com>
2025-01-29 05:02:06 +0000

    commit

---
+++ basics/conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33"
+//console.log(typeof score);
+
+let valueInNumber=Number(score);
+
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+//"33"-->33
+//"33abc"-->NaN(not a number)
+//true-->1
+//false-->0
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1-->true
+//0-->false
+//""-->false
+//"abhishek"--true
+
+let number=33
+
+let stringNumber=String(number)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
